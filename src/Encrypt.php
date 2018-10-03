a1ebde5fbfc6795244d508d142dd3df564362240
Amir Kalantar <amir.kalantar@gmail.com>
2018-10-03 18:29:48 +0330

    change encript and decript method

--- src/Encrypt.php
+++ src/Encrypt.php
@@ -19,14 +19,14 @@ class Encrypt
         $ivlen = openssl_cipher_iv_length($this->cipher);
         $iv = openssl_random_pseudo_bytes($ivlen);
         $ciphertext = openssl_encrypt($string, $this->cipher, $this->key, $options = 0, $iv);
-        return base64_encode(json_encode(['iv' => $iv, 'value' => $ciphertext]));
+        return base64_encode(json_encode(['iv' => base64_encode($iv), 'value' => $ciphertext]));
     }
 
     public function decrypt($string)
     {
         $data = base64_decode($string);
         $data = json_decode($data, true);
-        $iv = $data['iv'];
+        $iv = base64_decode($data['iv']);
         $value = $data['value'];
         return openssl_decrypt($value, $this->cipher, $this->key, $options = 0, $iv);
     }
